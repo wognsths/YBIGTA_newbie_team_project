21ffd156ff9d30b292807936951ba7cff61bb039
wognsths <2021122006@yonsei.ac.kr>
2025-01-23 20:15:20 +0900

    main.py도 추가

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -3,17 +3,18 @@ import glob
 from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.preprocessing.base_processor import BaseDataProcessor
-from review_analysis.preprocessing.example_processor import ExampleProcessor
-
+from review_analysis.preprocessing.naver_processor import NaverProcessor
+from review_analysis.preprocessing.imdb_processor import ImdbProcessor
 
 # 모든 preprocessing 클래스를 예시 형식으로 적어주세요. 
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
 PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
-    "reviews_example": ExampleProcessor,
-    # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
+    "reviews_naver": NaverProcessor,
+    "reviews_imdb": ImdbProcessor
+    # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naveWr.csv -> reviews_naver
 }
 
-REVIEW_COLLECTIONS = glob.glob(os.path.join("..","..","database", "reviews_*.csv"))
+REVIEW_COLLECTIONS = glob.glob(os.path.join("..", "..", "database", "reviews_*.csv"))
 
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser()
@@ -25,7 +26,6 @@ def create_parser() -> ArgumentParser:
     return parser
 
 if __name__ == "__main__":
-
     parser = create_parser()
     args = parser.parse_args()
 
